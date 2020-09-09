43064e7b28723b75af79e3cda88a25767fb8c941
Andre Peixoto <peixoto.amp@gmail.com>
2020-09-09 17:08:47 -0300

    initial commit - front-end development

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCkTdbki6SprST1oInSe3XpiHbu_q6ypSg",
+  authDomain: "clone-1cfc9.firebaseapp.com",
+  databaseURL: "https://clone-1cfc9.firebaseio.com",
+  projectId: "clone-1cfc9",
+  storageBucket: "clone-1cfc9.appspot.com",
+  messagingSenderId: "266771599576",
+  appId: "1:266771599576:web:a1b9f12fd50e907f9087ad",
+  measurementId: "G-8GFVKPMSQW"
+};
